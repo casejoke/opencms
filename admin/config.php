5e51b36c352c6f749be11f7b20b7f0aae77a1eee
Юрий Бреславец (d1gitable) <yuri.breslavets@d1gitable.ru>
2015-06-07 15:59:09 +0300

    add cms

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.opencms/admin/');
+define('HTTP_CATALOG', 'http://local.opencms/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.opencms/admin/');
+define('HTTPS_CATALOG', 'http://local.opencms/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/Macintosh_HD_2/mamp/local/opencms/admin/');
+define('DIR_SYSTEM', '/Volumes/Macintosh_HD_2/mamp/local/opencms/system/');
+define('DIR_LANGUAGE', '/Volumes/Macintosh_HD_2/mamp/local/opencms/admin/language/');
+define('DIR_TEMPLATE', '/Volumes/Macintosh_HD_2/mamp/local/opencms/admin/view/template/');
+define('DIR_CONFIG', '/Volumes/Macintosh_HD_2/mamp/local/opencms/system/config/');
+define('DIR_IMAGE', '/Volumes/Macintosh_HD_2/mamp/local/opencms/image/');
+define('DIR_CACHE', '/Volumes/Macintosh_HD_2/mamp/local/opencms/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/Macintosh_HD_2/mamp/local/opencms/system/download/');
+define('DIR_UPLOAD', '/Volumes/Macintosh_HD_2/mamp/local/opencms/system/upload/');
+define('DIR_LOGS', '/Volumes/Macintosh_HD_2/mamp/local/opencms/system/logs/');
+define('DIR_MODIFICATION', '/Volumes/Macintosh_HD_2/mamp/local/opencms/system/modification/');
+define('DIR_CATALOG', '/Volumes/Macintosh_HD_2/mamp/local/opencms/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'soos9Rah');
+define('DB_DATABASE', 'casejoke_opencms');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
